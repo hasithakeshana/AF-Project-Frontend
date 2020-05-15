c1c8fcd8cf92f8001de89bce6f97ee80527d08c9
Rashmika gamage <rashmikamadushan321@gmail.com>
2020-05-15 23:54:08 +0530

    view item created

--- src/components/ViewItem/ViewItem.js
+++ src/components/ViewItem/ViewItem.js
@@ -1,88 +1,90 @@
-import React,{ useState , useEffect } from 'react';
-
-import PropTypes from 'prop-types';
-
+import React, {useEffect, useState} from 'react';
 import {connect} from 'react-redux';
-
 import * as reduxActions from '../../common/actions';
-import Carousal from './Carousel';
 import queryString from 'query-string';
+import ReactLoading from 'react-loading';
 import Rating from '../Ratings/RatingsCom';
+import {forEach} from "react-bootstrap/cjs/ElementChildren";
 
 
-function ViewItem({getItemDetails,item,location, history}) {
+function ViewItem({getItemDetails, item, location, history}) {
 
     const [pID, setId] = useState("");
 
     useEffect(() => {
-    const values = queryString.parse(location.search);
-    setId(values.productId);
-    getItemDetails(values.productId); // SAGA ACTION
-    // const params = new URLSearchParams(location.search);
-  }, [location]);
+        const values = queryString.parse(location.search);
+        setId(values.productId);
+        getItemDetails(values.productId); // SAGA ACTION
+        // const params = new URLSearchParams(location.search);
+    }, [location]);
 
-   
-    return (
-        <div>
-        <div>
+    if(Object.entries(item).length !== 0){
+        console.log(item)
+        return (
 
-    <h4 class="alert-heading"> {item.name} </h4>
-            <Carousal></Carousal>
-            <br></br>
-            <br></br>
+            <div>
+                <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
+                    <div className="carousel-inner">
+                        <div className="carousel-item active">
+                            <img className="d-block w-100" src={"http://localhost:4000/uploads/"+   item.images[0].productImage} alt="First slide"/>
+                        </div>
+                        {
+                                item.images.slice(1).map(image =>
+                                    <div className="carousel-item">
+                                        <img className="d-block w-100" src={"http://localhost:4000/uploads/"+image.productImage}  alt=""/>
+                                    </div>
+                                )
+                        }
 
-          
+                    </div>
+                    <a className="carousel-control-prev" href="#carouselExampleControls" role="button"
+                       data-slide="prev">
+                        <span className="carousel-control-prev-icon" aria-hidden="true"/>
+                        <span className="sr-only">Previous</span>
+                    </a>
+                    <a className="carousel-control-next" href="#carouselExampleControls" role="button"
+                       data-slide="next">
+                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span className="sr-only">Next</span>
+                    </a>
+                </div>
+                <Rating product={item._id}/>
 
-            <p className="dark-grey-text">
-         {item.description}
-        </p>
-        <br></br>
-            
+            </div>
+        )
 
-        
-        <h5 class="alert-heading"> LKR {item.price}</h5>
+    }else{
 
-        <br></br>
-           
+        return(
+            <ReactLoading type={"spinningBubbles"} color={"#e75480"} height={'20%'} width={'20%'} />
+        );
 
-        <select class="form-control form-control-sm">
-                <option>Small</option>
-                 <option>Large</option>
-                 <option>Extra-Large</option>
-            </select>
+    }
 
-<br></br>
-<br></br>
-            <button type="button" class="btn btn-outline-danger">Add To Cart</button>
-            
-        </div>
-        <Rating product={item._id}></Rating>
-        </div>
-    )
 }
 
-  
-const mapStateToProps = (state)=> {
 
-  
-    return{
-    
-      item : state.item.currentSelectedItem,
-    }
-  };
-  
-  
-  const mapDispachToProps = (dispach) => {
-  
+const mapStateToProps = (state) => {
+
+
     return {
-     
-     
-     getItemDetails : (ProductId) => dispach(reduxActions.GetViewItemAction(ProductId)),
-  
-  
+
+        item: state.item.currentSelectedItem,
     }
-  }
-  
-  export default connect(mapStateToProps,mapDispachToProps) (ViewItem);
+};
+
+
+const mapDispachToProps = (dispach) => {
+
+    return {
+
+
+        getItemDetails: (ProductId) => dispach(reduxActions.GetViewItemAction(ProductId)),
+
+
+    }
+}
+
+export default connect(mapStateToProps, mapDispachToProps)(ViewItem);
 
 
