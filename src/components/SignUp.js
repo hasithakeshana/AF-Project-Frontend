ed0ec95baaf1fb3672bcd57a626aab06c3b08eb1
hasitha <hasithakeshana9900@gmail.com>
2020-05-24 12:28:23 +0530

    add

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -10,7 +10,7 @@ import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
-import {makeStyles} from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 
 
@@ -20,185 +20,213 @@ import {connect} from 'react-redux';
 
 import * as reduxActions from '../common/actions';
 
+import { useFormik } from 'formik';
+import * as Yup from "yup";
+
 function Copyright() {
-    return (
-        <Typography variant="body2" color="textSecondary" align="center">
-        {'Copyright © '}
-    <Link color="inherit" href="https://material-ui.com/">
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
         Your Website
-    </Link>{' '}
-    {new Date().getFullYear()}
-    {'.'}
-</Typography>
-    );
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
 }
 
 const useStyles = makeStyles((theme) => ({
-    paper: {
-        marginTop: theme.spacing(8),
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-    },
-    avatar: {
-        margin: theme.spacing(1),
-        backgroundColor: theme.palette.secondary.main,
-    },
-    form: {
-        width: '100%', // Fix IE 11 issue.
-        marginTop: theme.spacing(3),
-    },
-    submit: {
-        margin: theme.spacing(3, 0, 2),
-    },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  }
 }));
+
+const validationSchema = Yup.object({
+  firstName: Yup.string().required("Please fill the field"),
+  lastName: Yup.string().required("Please fill the field"),
+  email: Yup.string().email("Please enter a valid email").required("Please fill the field"),
+  password: Yup.string().min(8).required("Please fill the field"),
+  confirm_password: Yup.string().required("Please fill the field").oneOf([Yup.ref('password'), null], 'Passwords do not match',),
+});
+
+
 
 function SignUp({signUpuser}) {
 
-    const [firstName, setFname] = useState("");
-    const [lastName, setLname] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    const { handleSubmit, handleChange, values, errors } = useFormik({
+      initialValues: {
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        confirm_password: "",
+      },
+      validationSchema,
+      onSubmit(values) {
+          console.log(values);
+        
+      }
+    });
+    
+    const isDisabled = Object.keys(errors).some(x => errors[x]);
 
-    const handleSubmit = (evt) => {
-        evt.preventDefault();
-    }
 
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <Container component="main" maxWidth="xs">
-            <CssBaseline/>
-            <div className={classes.paper}>
-                <Avatar className={classes.avatar}>
-                    <LockOutlinedIcon/>
-                </Avatar>
-                <Typography component="h1" variant="h5">
-                    Sign up
-                </Typography>
-                <form className={classes.form} noValidate onSubmit={handleSubmit}>
-                    <Grid container spacing={2}>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                autoComplete="fname"
-                                name="firstName"
-                                variant="outlined"
-                                required
-                                fullWidth
-                                id="firstName"
-                                label="First Name"
-                                autoFocus
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <form className={classes.form} noValidate onSubmit={handleSubmit}>
+          <Grid container spacing={3}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                error
+                autoComplete="fname"
+                name="firstName"
+                variant="outlined"
+                fullWidth
+                id="firstName"
+                label="First Name"
+                autoFocus
+                onChange={handleChange}
+              />{errors.firstName}
+            </Grid>
+            
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                fullWidth
+                id="lastName"
+                label="Last Name"
+                name="lastName"
+                autoComplete="lname"
+                onChange={handleChange}
+              />{errors.lastName}
+            </Grid>
+            
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                onChange={handleChange}
+              />
+            </Grid>
+            {errors.email}
 
-                                value={firstName}
-                                onChange={e => setFname(e.target.value)}
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                variant="outlined"
-                                required
-                                fullWidth
-                                id="lastName"
-                                label="Last Name"
-                                name="lastName"
-                                autoComplete="lname"
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                onChange={handleChange}
+              />
+            </Grid>
+            {errors.password}
 
-                                value={lastName}
-                                onChange={e => setLname(e.target.value)}
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                variant="outlined"
-                                required
-                                fullWidth
-                                id="email"
-                                label="Email Address"
-                                name="email"
-                                autoComplete="email"
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                fullWidth
+                name="confirm_password"
+                label="Confirm Password"
+                type="password"
+                id="confirm_password"
+                onChange={handleChange}
+              />
+            </Grid>
+            {errors.confirm_password}
 
-                                value={email}
-                                onChange={e => setEmail(e.target.value)}
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <TextField
-                                variant="outlined"
-                                required
-                                fullWidth
-                                name="password"
-                                label="Password"
-                                type="password"
-                                id="password"
-                                autoComplete="current-password"
+            <Grid item xs={12}>
+              <FormControlLabel
+                control={<Checkbox value="allowExtraEmails" color="primary" />}
+                label="I want to receive inspiration, marketing promotions and updates via email."
+              />
+            </Grid>
+          </Grid>
 
-                                value={password}
-                                onChange={e => setPassword(e.target.value)}
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <FormControlLabel
-                                control={<Checkbox value="allowExtraEmails" color="primary"/>}
-                                label="I want to receive inspiration, marketing promotions and updates via email."
-                            />
-                        </Grid>
-                    </Grid>
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        className={classes.submit}
-
-                        onClick={() => signUpuser(firstName, lastName, email, password)}
-                    >
-                        Sign Up
-                    </Button>
-                    <Grid container justify="flex-end">
-                        <Grid item>
-                            <Link href="#" variant="body2">
-                                Already have an account? Sign in
-                            </Link>
-                        </Grid>
-                    </Grid>
-                </form>
-            </div>
-            <Box mt={5}>
-                <Copyright/>
-            </Box>
-        </Container>
-    );
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            disabled={isDisabled}
+            onClick = {  () => signUpuser(values.firstName,values.lastName,values.email,values.password)  }
+          >
+            Sign Up
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Link href="#" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={5}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
 }
 
 SignUp.propTypes = {
-
+  
     signUpuser: PropTypes.func,
-};
-
-
-const mapStateToProps = (state) => {
-
-    console.log('state', state);
-    return {
-
-        emails: state.email
+  };
+  
+  
+  
+  const mapStateToProps = (state)=> {
+  
+    console.log('state',state);
+    return{
+     
+      emails : state.email
     }
-};
-
-
-const mapDispachToProps = (dispach) => {
-
+  };
+  
+  
+  const mapDispachToProps = (dispach) => {
+  
     return {
-
-        signUpuser: (firstName, lastName, email, password) => dispach(reduxActions.signUpAction({
-            firstName,
-            lastName,
-            email,
-            password
-        })),
-
+     
+      signUpuser : (firstName,lastName,email,password) => dispach(reduxActions.signUpAction({firstName,lastName,email,password})) ,
+  
+  
     }
-}
+  }
+  
 
-
-export default connect(mapStateToProps, mapDispachToProps)(SignUp);
+  export default connect(mapStateToProps,mapDispachToProps) (SignUp);
+    
